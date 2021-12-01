c30d21cea226c1a063c41a9a6056a9d61493e937
roynatech2544 <whiteshell2544@naver.com>
2021-12-01 17:24:59 +0900

    treewide: derp-ify
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,18 +1,10 @@
-git clone  git@github.com:eurekadevelopment/android_vendor_samsung_a20_r.git vendor/samsung -b master
-#git clone git@github.com:eurekadevelopment/android_device_samsung_a20_r.git device/samsung
+git clone https://github.com/DerpFest-Devices/vendor_samsung_a20-a20e vendor/samsung
 rm -rf hardware/samsung
-git clone git@github.com:eurekadevelopment/android_hardware_samsung.git hardware/samsung -b NUKE_ALL
-git clone git@github.com:eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git kernel/samsung/exynos7885 --depth=1 -b R8.0_rom
+git clone https://github.com/eurekadevelopment/android_hardware_samsung hardware/samsung -b NUKE_ALL
+git clone https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S kernel/samsung/exynos7885 --depth=1
 git clone https://github.com/lineageos/android_hardware_samsung_nfc hardware/samsung/nfc
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
-if test -f device/samsung/universal7885-common/vendor_name; then
-rm device/samsung/universal7885-common/vendor_name
-fi
-python3 device/samsung/universal7885-common/vendor_detect/main.py -d 0
-echo "Generating A20 Makefiles"
-./device/samsung/a20/setup.sh
-echo "Generating A20e Makefiles"
-./device/samsung/a20e/setup.sh
+
 
